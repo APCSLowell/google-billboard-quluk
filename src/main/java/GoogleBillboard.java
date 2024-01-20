cf1be0f7b29def5addb881fdf0249425d5459c7a
quluk <143124115+quluk@users.noreply.github.com>
2024-01-19 21:15:29 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,12 +3,14 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+for(int i = 2; i < (e.length() - 10); i++){ 
 	double a = Double.parseDouble(e.substring(i, i+10)); 
 	if(isPrime(a) == true){ 
-		System.out.println(a); break; 
+		System.out.println(a); 
+		break; 
+	} 
+} 
 	}
-	
-
 	//Finish this function
 	public static boolean isPrime(double d){
 		    if (d <= 1) {
@@ -23,10 +25,4 @@ public class GoogleBillboard {
 	}
 
 }
-for(int i = 2; i < (e.length() - 10); i++){ 
-	double a = Double.parseDouble(e.substring(i, i+10)); 
-	if(isPrime(a) == true){ 
-		System.out.println(a); break; 
-	} 
-} 
 
